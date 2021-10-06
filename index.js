8cad06af674f659e2ba91c59abad5ec532ba7783
aldoortiz128 <aldoortiz128@gmail.com>
2021-10-06 19:11:07 +0000

    Finished

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 //1
-function fizzBuzz(){
-  
+function fizzBuzz(num){
+    //Log all the numbers from 1 to 100.
+    //If the number is a multiple of 3, log "Fizz" instead.
+    //If the number is a mutltiple of 5, log "Buzz" instead.
+    //If the number is a multiple of 3 & 5, log "Fizzbuzz" instead.
+    
+    for (let i = 0; i <= num; i++) {
+    if (i % 3 & i % 5) {console.log ("fizzbuzz");} 
+    // Must be first condition checked in the order of execution
+    // for program to run properly
+    
+    else if (i % 3) {console.log ("fizz"); }
+    else if (i % 5) {console.log ("buzz");}
+    {console.log (i);}
+}
 }
 
-//2
-function fizzBuzz(number){
-  
-}
+
+
+fizzBuzz(25);
 
 
